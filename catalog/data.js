8e5f73a0da68c84f50419961ab92eeee59eded1e
ALEXEY <116389283+Al546507@users.noreply.github.com>
2025-08-20 18:39:09 +0200

    Create data.js

---
+++ catalog/data.js
@@ -0,0 +1,22 @@
+const products = [
+  {
+    name: "Кирпич строительный",
+    price: "25 ₽/шт",
+    image: "https://upload.wikimedia.org/wikipedia/commons/5/57/Red_bricks.jpg"
+  },
+  {
+    name: "Цемент М500",
+    price: "390 ₽/мешок (50 кг)",
+    image: "https://upload.wikimedia.org/wikipedia/commons/0/0c/Cement_bag.jpg"
+  },
+  {
+    name: "Песок строительный",
+    price: "1200 ₽/м³",
+    image: "https://upload.wikimedia.org/wikipedia/commons/6/6e/Sandpile.jpg"
+  },
+  {
+    name: "Щебень гранитный",
+    price: "1500 ₽/м³",
+    image: "https://upload.wikimedia.org/wikipedia/commons/f/f1/Gravel_pile.jpg"
+  }
+];
